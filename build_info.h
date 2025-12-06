390a3d05131c6aa15f352fd878c63410f223b878
Kumar Challa <chvvkumar@users.noreply.github.com>
2025-12-06 12:24:38 -0600

    feat: enhance watchdog reset handling during long operations in WiFi setup and QR code display

--- build_info.h
+++ build_info.h
@@ -9,8 +9,8 @@
 #define BUILD_TIME __TIME__
 
 // Git information (updated by compile script)
-#define GIT_COMMIT_HASH "8b63735"
-#define GIT_COMMIT_FULL "8b637358b6e2b26fe5149e043e5415754c9825e5"
+#define GIT_COMMIT_HASH "2e13bc9"
+#define GIT_COMMIT_FULL "2e13bc965bdd5e2be0129c9d499b441f1a55f644"
 #define GIT_BRANCH "snd"
 
 #endif // BUILD_INFO_H
