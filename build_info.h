027d414b82984ae62b2737294ebb04a046be1019
Kumar Challa <chvvkumar@users.noreply.github.com>
2025-12-08 14:07:27 -0600

    fix: update build information and adjust console page layout for wider console area

--- build_info.h
+++ build_info.h
@@ -9,8 +9,8 @@
 #define BUILD_TIME __TIME__
 
 // Git information (updated by compile script)
-#define GIT_COMMIT_HASH "1e8be4d"
-#define GIT_COMMIT_FULL "1e8be4d163f61fecc1ae0411f1d1ede5ff9ffc92"
+#define GIT_COMMIT_HASH "3b4da62"
+#define GIT_COMMIT_FULL "3b4da62ab417b00230a1923a094283a0b19f801a"
 #define GIT_BRANCH "snd"
 
 #endif // BUILD_INFO_H
