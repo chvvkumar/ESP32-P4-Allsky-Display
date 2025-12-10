cc0d04c7781d6b84e8e469030e33cd6f97aa685b
Kumar Challa <chvvkumar@users.noreply.github.com>
2025-12-10 08:05:19 -0600

    feat: add configuration URL and readable uptime to MQTT device discovery

--- build_info.h
+++ build_info.h
@@ -9,8 +9,8 @@
 #define BUILD_TIME __TIME__
 
 // Git information (updated by compile script)
-#define GIT_COMMIT_HASH "1474580"
-#define GIT_COMMIT_FULL "1474580f96385ee00dfc9642676c15a0ce39fffc"
+#define GIT_COMMIT_HASH "55ed106"
+#define GIT_COMMIT_FULL "55ed106dc8c69a2f6bc5ee33c6c8955466712b47"
 #define GIT_BRANCH "snd"
 
 #endif // BUILD_INFO_H
