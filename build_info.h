e2b27c0b412cbf8757b32d37811a3c7f1af00ab0
Kumar Challa <chvvkumar@users.noreply.github.com>
2025-12-11 21:37:29 -0600

    feat(ha_discovery): optimize topic handling with cached strings to prevent memory leaks

--- build_info.h
+++ build_info.h
@@ -9,8 +9,8 @@
 #define BUILD_TIME __TIME__
 
 // Git information (updated by compile script)
-#define GIT_COMMIT_HASH "0d5c9ee"
-#define GIT_COMMIT_FULL "0d5c9eeb9b2e1bc88334dcaeab7f7db9b0ab22ee"
+#define GIT_COMMIT_HASH "1abced7"
+#define GIT_COMMIT_FULL "1abced7e4005d1419479304cdb6672083485dd6d"
 #define GIT_BRANCH "snd"
 
 #endif // BUILD_INFO_H
