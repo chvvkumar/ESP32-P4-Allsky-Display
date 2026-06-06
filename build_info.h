f222b3e8d2ab500c6f058c359bb5436e47a4489a
Kumar Challa <chvvkumar@users.noreply.github.com>
2026-06-06 10:31:08 -0500

    feat: refine moon ui

--- build_info.h
+++ build_info.h
@@ -9,8 +9,8 @@
 #define BUILD_TIME __TIME__
 
 // Git information (updated by compile script)
-#define GIT_COMMIT_HASH "59704cd"
-#define GIT_COMMIT_FULL "59704cd3769cdac73591658ac09376021dad8704"
+#define GIT_COMMIT_HASH "8845feb"
+#define GIT_COMMIT_FULL "8845feb8cfedc9e828d4ac05d499daeab1c3773e"
 #define GIT_BRANCH "feat/solar-lunar-display"
 
 #endif // BUILD_INFO_H
