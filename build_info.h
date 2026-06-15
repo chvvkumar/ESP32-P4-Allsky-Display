e79c8e40338dd8b0ee1248c797d60877fdca72ec
Kumar Challa <chvvkumar@users.noreply.github.com>
2026-06-14 20:34:32 -0500

    frst: add backup and restore

--- build_info.h
+++ build_info.h
@@ -9,8 +9,8 @@
 #define BUILD_TIME __TIME__
 
 // Git information (updated by compile script)
-#define GIT_COMMIT_HASH "8845feb"
-#define GIT_COMMIT_FULL "8845feb8cfedc9e828d4ac05d499daeab1c3773e"
-#define GIT_BRANCH "feat/solar-lunar-display"
+#define GIT_COMMIT_HASH "1f1abd5"
+#define GIT_COMMIT_FULL "1f1abd5bdb01707a96e570f90a548c52461d36c0"
+#define GIT_BRANCH "snd"
 
 #endif // BUILD_INFO_H
